2fb45ce578f65b32bf9e670ecac8d035c4c8f9ad
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-02-04 19:31:21 +0100

    Updated app.ts to return the main section.
    App.ts now returns the main section to be able to manipulate it within TS.
    
     Conflicts:
    	src/pages/parts/app.ts

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -2,8 +2,15 @@ import { head } from "./head";
 import { createHeaderContent } from "../../helpers/HTMLHeader";
 import { headerMainContent } from "./header";
 
-export const application = (title: HTMLTitleElement["innerHTML"]) => {
-  head(title);
-  const headerElement = createHeaderContent(headerMainContent);
-  document.body.appendChild(headerElement);
+export const application = (title: HTMLTitleElement['innerHTML']) => {
+    const app = document.createElement('main'), headerElement = createHeaderContent(headerMainContent);
+
+    head(title);
+    
+    Object.assign(app, {
+        id: 'app'
+    });
+    
+    document.body.append(headerElement, app);
+    return app;
 };
