ce8d91d98bb37d6ca3bb6b2fc3b8d4093a3ec3dd
CJVIK <carl.viklund@hotmail.com>
2025-02-05 13:29:38 +0100

    Optimized createHeaderContent function
    Optimized function createHeaderContent in src/Helpers/HTMLHeader.ts
    inline with Ymer's code style, pretty neat.
    Updated src/pages/parts/header.ts to reflect the changes, and as export const.

---
+++ src/pages/parts/header.ts
@@ -0,0 +1,10 @@
+export const headerMainContent = {
+    sectionTitle: {
+        h1: "FRAMSIDAN",
+        p: "RIP LUKAs FIGMA - som kom till god användning i denna uppgift"
+    },
+    sectionDiscription: {
+        p1: "Det är inte bara",
+        p2: "Insidan som räknas"
+    }
+}
