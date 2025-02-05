43198eaf162a9ca7bb082ddbd544ff81aaba8930
CJVIK <carl.viklund@hotmail.com>
2025-02-05 13:41:11 +0100

    Added structure to the header for welcome pages
    Made header a separate component and added a header to the welcome page, this way you can make diffrent headers for diffrent pages.

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -1,5 +1,9 @@
 import { head } from './head';
+import { createHeaderContent } from '../../Helpers/HTMLHeader';
+import { headerMainContent } from './header';
 
 export const application = (title: HTMLTitleElement['innerHTML']) => {
-    head(title);  
+    head(title);
+    const headerElement = createHeaderContent(headerMainContent);
+    document.body.appendChild(headerElement);
 };
