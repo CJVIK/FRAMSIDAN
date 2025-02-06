ed26a03b9dfa6fed92c8484ea947e750f2538f05
Anna <anna.viklund1@medieinstitutet.se>
2025-02-06 15:54:14 +0100

    Conflict solved
    Solved conflict in app.ts
    
    Updated app.ts to return the main section.
    
    App.ts now returns the main section to be able to manipulate it within TS.

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -2,8 +2,17 @@ import { head } from "./head";
 import { createHeaderContent } from "../../helpers/HTMLHeader";
 import { headerMainContent } from "./header";
 
-export const application = (title: HTMLTitleElement["innerHTML"]) => {
-  head(title);
+export const application = (title: HTMLTitleElement['innerHTML']) => {
+    const app = document.createElement('main');
+
+    Object.assign(app, {
+        id: 'app'
+    });
+
+    head(title);
+
+    
   const headerElement = createHeaderContent(headerMainContent);
   document.body.appendChild(headerElement);
+  return app;
 };
