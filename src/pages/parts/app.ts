b68629166946b21fcffe5e6f8bc3d7cf797c9d3e
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-02-06 14:38:18 +0100

    Solved conflict
    Open conflict left unsolved is now solved.
    Changes to be committed:
    	modified:   src/pages/parts/app.ts

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import { head } from "./head";
 import { createHeaderContent } from "../../helpers/HTMLHeader";
 import { headerMainContent } from "./header";
@@ -15,16 +13,7 @@ export const application = (title: HTMLTitleElement['innerHTML']) => {
     head(title);
     const headerElement = createHeaderContent(headerMainContent);
     document.body.appendChild(headerElement);
- footerHTML();
+    
+    footerHTML();
     return app;
 };
-
-=======
-import { footerHTML } from '../../HTML/footer';
-import { head } from './head';
-
-export const application = (title: HTMLTitleElement['innerHTML']) => {
-    head(title);
-    footerHTML();
-};
->>>>>>> 982341599cb31189f9fd5c21ae71e5bbba727b8d
