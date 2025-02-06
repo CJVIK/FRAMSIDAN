8914b43d4f1a6f4fbba6fcae6046d87be50557e6
Anna <anna.viklund1@medieinstitutet.se>
2025-02-06 15:58:35 +0100

    Removed comments
    Comments in start.ts is now gone

--- src/pages/start.ts
+++ src/pages/start.ts
@@ -3,18 +3,6 @@ import { application } from "./parts/app";
 const main = application("Start page");
 document.body.append(main);
 
-// const heroContainer = document.createElement("div");
-// heroContainer.classList.add("hero");
-
-// const heroPicture = document.createElement("div");
-// heroPicture.classList.add("heroBackground");
-
-// main.appendChild(heroContainer);
-// heroContainer.appendChild(heroPicture);
-// console.log(document.body.innerHTML);
-// console.log(main);
-
-
 if (main) {
     const heroContainer = document.createElement("div");
     heroContainer.classList.add("hero");
