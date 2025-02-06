ffbe92faa931c5f1abfac2919b947fcdfca4aac1
CJVIK <carl.viklund@hotmail.com>
2025-02-06 13:17:10 +0100

    Corrected file name and path
    Changed file name of helper folder - and updated path

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -1,9 +1,9 @@
-import { head } from './head';
-import { createHeaderContent } from '../../helpers/HTMLHeader';
-import { headerMainContent } from './header';
+import { head } from "./head";
+import { createHeaderContent } from "../../helpers/HTMLHeader";
+import { headerMainContent } from "./header";
 
-export const application = (title: HTMLTitleElement['innerHTML']) => {
-    head(title);
-    const headerElement = createHeaderContent(headerMainContent);
-    document.body.appendChild(headerElement);
+export const application = (title: HTMLTitleElement["innerHTML"]) => {
+  head(title);
+  const headerElement = createHeaderContent(headerMainContent);
+  document.body.appendChild(headerElement);
 };
