1762458963c9c0f995f25390984c29742dbf45f3
hjortie <elinhjortskull@live.se>
2025-02-07 14:51:52 +0100

    Nav-link to page Cases.html provided with href.
    Updated the name of the mainSection.ts to cases.ts and moved it from directory "parts" to "pages".

--- src/pages/parts/footer.ts
+++ src/pages/parts/footer.ts
@@ -1,20 +1,19 @@
-
-
 export const footer = () => {
-    const nav = document.createElement("nav");
-    nav.id = "nav";
-    const footer = document.createElement("footer");
+  const nav = document.createElement("nav");
+  nav.id = "nav";
+  const footer = document.createElement("footer");
 
-    footer.appendChild(nav);
+  footer.appendChild(nav);
 
-    const headingCases = document.createElement("a");
-    headingCases.innerHTML = "Cases";
-    const headingTheTeam = document.createElement("a");
-    headingTheTeam.innerHTML = "The Team";
-    const headingContact = document.createElement("a");
-    headingContact.innerHTML = "Contact";
+  const headingCases = document.createElement("a");
+  headingCases.href = "/src/pages/cases.html";
+  headingCases.innerText = "Cases";
+  const headingTheTeam = document.createElement("a");
+  headingTheTeam.innerHTML = "The Team";
+  const headingContact = document.createElement("a");
+  headingContact.innerHTML = "Contact";
 
-    nav.append(headingCases, headingTheTeam, headingContact)
+  nav.append(headingCases, headingTheTeam, headingContact);
 
-    return footer;
-} 
+  return footer;
+};
