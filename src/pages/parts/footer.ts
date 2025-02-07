d7d2ba95f8c08f5fbf7eabe7f36d29ff1673ce2e
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-02-07 16:29:02 +0100

    Updated team page.
    Updated the teampage with functional teamcard and added som links in the
    nav to make it possible to navigate through ell existing pages.

--- src/pages/parts/footer.ts
+++ src/pages/parts/footer.ts
@@ -5,15 +5,19 @@ export const footer = () => {
 
   footer.appendChild(nav);
 
+  const linkHome = document.createElement("a");
+  linkHome.href = "/";
+  linkHome.innerText = "Home";
   const linkCases = document.createElement("a");
   linkCases.href = "/pages/cases.html";
   linkCases.innerText = "Cases";
   const linkTheTeam = document.createElement("a");
+  linkTheTeam.href = "/pages/team.html";
   linkTheTeam.innerHTML = "The Team";
   const linkContact = document.createElement("a");
   linkContact.innerHTML = "Contact";
 
-  nav.append(linkCases, linkTheTeam, linkContact);
+  nav.append(linkHome, linkCases, linkTheTeam, linkContact);
 
   return footer;
 };
