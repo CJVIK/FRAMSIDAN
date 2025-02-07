4cde4ede8bb38092374de2c5b8c48ae1c43eadd9
hjortie <elinhjortskull@live.se>
2025-02-07 08:23:59 +0100

    Added some content to the main section of cases.html
    Re-commit from new clone: added content to cases.html, assets to be displayed in the flipcards will be added in separate commit.

--- src/pages/parts/mainSection.ts
+++ src/pages/parts/mainSection.ts
@@ -1,15 +1,21 @@
-const mainSection = document.getElementById("main");
-if (mainSection) {
-  const slideShow = document.createElement("section");
-  const slideCardContainer = document.createElement("div");
-  const slideCard = document.createElement("img");
+import { HTMLHelper } from "../../helpers/HTMLHelper";
+import { application } from "./app";
 
-  slideShow.className = "slideshow-container";
-  slideCardContainer.className = "slidecard-container";
-  slideCard.className = "slidecard-image";
-  slideCard.src = "../assets/puss.jpg";
+const case1 = {
+  front: `<img src="../assets/API-omdb.png" alt="En applikation för filmer">`,
+  back: "<p>En applikation för filmer</p>",
+};
+const case2 = {
+  front: `<img src="../assets/konst-i-konsten.png" alt="En applikation för 'konst i konsten'">`,
+  back: "<p>En applikation för 'konst i konsten'</p>",
+};
+const case3 = {
+  front: `<img src="../assets/the-todos.png" alt="En applikation för to-do-listor">`,
+  back: "<p>En applikation för to-do-listor</p>",
+};
 
-  slideCardContainer.appendChild(slideCard);
-  slideShow.appendChild(slideCardContainer);
-  mainSection.appendChild(slideShow);
-}
+const cases = [case1, case2, case3];
+
+const main = application("Cases");
+
+main.append(HTMLHelper.createFlipCards(cases));
