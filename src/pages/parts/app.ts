6e15c88626279ae18968dd4af8432c5ad0ab4eb5
isabellavelasquez <isabella.velasquez1998@gmail.com>
2025-02-07 10:06:28 +0100

    Hotfix on app.ts
    Removed duplicated variables and function callings
    Changed "appendChild" to "append" on line 9

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -1,23 +1,18 @@
 import { head } from "./head";
-import { createHeaderContent } from "../../helpers/HTMLHeader";
+import { createHeaderContent } from "../../Helpers/HTMLHeader";
 import { headerMainContent } from "./header";
 import { footerHTML } from '../../HTML/footer';
 
 export const application = (title: HTMLTitleElement['innerHTML']) => {
-    const app = document.createElement('main'), headerElement = createHeaderContent(headerMainContent);
-
+    const app = document.createElement('main') 
+    const headerElement = createHeaderContent(headerMainContent);
+    document.body.append(headerElement, app);
     head(title);
     
     Object.assign(app, {
         id: 'app'
     });
-
-
-    head(title);
-    const headerElement = createHeaderContent(headerMainContent);
-    document.body.appendChild(headerElement, app);
     
     footerHTML();
-
     return app;
 };
