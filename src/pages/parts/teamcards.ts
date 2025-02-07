45d3fb0130c5529c3e2a62cc8272aa3bc6b8386c
CJVIK <carl.viklund@hotmail.com>
2025-02-07 13:58:47 +0100

    Changed cjCard to export
    since i need to export it to app.ts

--- src/pages/parts/teamcards.ts
+++ src/pages/parts/teamcards.ts
@@ -1,4 +1,4 @@
-const cjCard: { front: string; back: string } = {
+export const cjCard: { front: string; back: string } = {
     front: '<img src="" alt="CJ avatar image"> <h3>CJ</h3>',
     back: '<p>Hi, I am CJ. Contact down below</p><a href="https://www.linkedin.com/in/carl-johan-viklund-b70b79335/">LinkedIn</a><a href="https://steamcommunity.com/profiles/76561197976600830/">Steam</a>'
 };
