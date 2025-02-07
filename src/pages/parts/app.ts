d5fc8647842fa4bd4e05a8ecc5187d42c5108bbf
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-02-07 10:27:16 +0100

    Merge pull request #24 from CJVIK/hotfix-header-structure
    Hotfix on app.ts

--- src/pages/parts/app.ts
+++ src/pages/parts/app.ts
@@ -3,6 +3,7 @@ import { createHeaderContent } from "../../helpers/HTMLHeader";
 import { headerMainContent } from "./header";
 
 export const application = (title: HTMLTitleElement['innerHTML']) => {
+<<<<<<< HEAD
     const app = document.createElement('main');
 
     Object.assign(app, {
@@ -13,5 +14,18 @@ export const application = (title: HTMLTitleElement['innerHTML']) => {
     const headerElement = createHeaderContent(headerMainContent);
     document.body.appendChild(headerElement);
   
+=======
+    const app = document.createElement('main'), headerElement = createHeaderContent(headerMainContent);
+    
+    head(title);
+    
+    Object.assign(app, {
+        id: 'app'
+    });
+    
+    document.body.append(headerElement, app);
+    footerHTML();
+
+>>>>>>> f17c94a (Merge pull request #24 from CJVIK/hotfix-header-structure)
     return app;
 };
