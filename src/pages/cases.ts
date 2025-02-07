c38078a9e22ebdca3b3afa0748dee237b884e329
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2025-02-07 15:48:08 +0100

    Updated path for images.
    updated path to images to make it easier to understand from the file
    structure.

--- src/pages/cases.ts
+++ src/pages/cases.ts
@@ -2,11 +2,11 @@ import { HTMLHelper } from "../helpers/HTMLHelper";
 import * as app from "./parts/app";
 
 const case1 = {
-  front: `<img src="../assets/API-omdb.png" alt="En applikation för filmer">`,
+  front: `<img src="/src/img/API-omdb.png" alt="En applikation för filmer">`,
   back: "<p>En applikation för filmer</p>",
 };
 const case2 = {
-  front: `<img src="../assets/the-todos.png" alt="En applikation för to-do-listor">`,
+  front: `<img src="/src/img/the-todos.png" alt="En applikation för to-do-listor">`,
   back: "<p>En applikation för to-do-listor</p>",
 };
 
